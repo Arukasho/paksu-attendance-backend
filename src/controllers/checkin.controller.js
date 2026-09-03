874fbec2de3d820e0a508ce39f2b149f3d8a880d
Arukasho <alwijeremy@gmail.com>
2026-09-03 17:47:18 +0700

    change qr payload

--- src/controllers/checkin.controller.js
+++ src/controllers/checkin.controller.js
@@ -3,14 +3,15 @@ const { processCheckin } = require("../services/checkin.service");
 async function checkin(req, res, next) {
   const { qr_payload } = req.body;
 
-  if (qr_payload !== "ORG_ATTENDANCE_CHECKIN") {
-    return res
-      .status(400)
-      .json({
-        error: true,
-        code: "invalid_qr_payload",
-        message: "Unrecognized QR code.",
-      });
+  if (
+    qr_payload !==
+    "https://docs.google.com/forms/d/e/1FAIpQLSdaNIkOYTvyNW84dOGHU6UiG8X3tzfOlKAMO4TnJM3MTGcywQ/viewform?usp=header"
+  ) {
+    return res.status(400).json({
+      error: true,
+      code: "invalid_qr_payload",
+      message: "Unrecognized QR code.",
+    });
   }
 
   try {
